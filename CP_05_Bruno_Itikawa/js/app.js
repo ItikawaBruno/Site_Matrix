3b1543bc9c68835c7363607a3869fcfee3c778c3
ItikawaBruno <brunoitikawa@gmail.com>
2024-10-18 19:58:47 -0300

    Add files via upload

---
+++ CP_05_Bruno_Itikawa/js/app.js
@@ -0,0 +1,162 @@
+let img=document.querySelector('#mostrar')
+let mostrarDados=document.querySelector('#mostrarDados')
+let respostaDados=''
+let respostaImg=''
+
+
+respostaImg+=`
+    <img id="img1" src="./image/product-detail-1.jpg" 
+    alt="relogio vermelho tombado de lado com mostrando as horas fundo da imagem é branca">
+    `
+
+    respostaDados+=`
+         <div class="box-stars">
+         <i class="bi bi-star-fill"></i>
+         <i class="bi bi-star-fill"></i>
+        <i class="bi bi-star-fill"></i>
+        <i class="bi bi-star-fill"></i>
+        </div>
+         <h3>Red digital watch</h3>
+         <h5>R$ 9999,99</h5>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Cum velit reprehenderit reiciendis quae incidunt nostrum ipsa vero autem quisquam libero.</p>
+         <a href="" class="link-btn">Adicionar ao carrinho</a>
+         <a href=""><i class="bi bi-heart"></i>Adicionar a lista e desejos</a>
+    `
+
+
+    img.innerHTML=respostaImg
+    mostrarDados.innerHTML=respostaDados
+
+
+
+
+
+document.querySelector('#img1').addEventListener('click',()=>{
+ respostaDados=''
+ respostaImg=''
+
+    respostaImg+=`
+    <img id="img1" src="./image/product-detail-1.jpg" 
+    alt="relogio vermelho tombado de lado com mostrando as horas fundo da imagem é branca">
+    `
+
+    respostaDados+=`
+         <div class="box-stars">
+         <i class="bi bi-star-fill"></i>
+         <i class="bi bi-star-fill"></i>
+        <i class="bi bi-star-fill"></i>
+        <i class="bi bi-star-fill"></i>
+        </div>
+         <h3>Red digital watch</h3>
+         <h5>R$ 9999,99</h5>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Cum velit reprehenderit reiciendis quae incidunt nostrum ipsa vero autem quisquam libero.</p>
+         <a href="" class="link-btn">Adicionar ao carrinho</a>
+         <a href=""><i class="bi bi-heart"></i>Adicionar a lista e desejos</a>
+    `
+
+
+    img.innerHTML=respostaImg
+    mostrarDados.innerHTML=respostaDados
+    
+})
+
+
+document.querySelector('#img2').addEventListener('click',()=>{
+     respostaDados=''
+ respostaImg=''
+
+    respostaImg+=`
+    <img id="img1" src="./image/product-detail-2.jpg" 
+    alt="relógio todo preto virado de frente em pé  mostrando a horas.">
+    `
+
+    respostaDados+=`
+         <div class="box-stars">
+         <i class="bi bi-star-fill"></i>
+         <i class="bi bi-star-fill"></i>
+        <i class="bi bi-star-fill"></i>
+        <i class="bi bi-star-fill"></i>
+        </div>
+         <h3>All Black digital watch</h3>
+         <h5>R$ 9999,99</h5>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Cum velit reprehenderit reiciendis quae incidunt nostrum ipsa vero autem quisquam libero.</p>
+         <a href="" class="link-btn">Adicionar ao carrinho</a>
+         <a href=""><i class="bi bi-heart"></i>Adicionar a lista e desejos</a>
+    `
+
+
+    img.innerHTML=respostaImg
+    mostrarDados.innerHTML=respostaDados
+    
+})
+
+
+document.querySelector('#img3').addEventListener('click',()=>{
+     respostaDados=''
+ respostaImg=''
+
+    respostaImg+=`
+    <img id="img1" src="./image/product-detail-3.jpg" 
+    alt="relógio preto com o pulso prateado deitado mostrando as horas.">
+    `
+
+    respostaDados+=`
+         <div class="box-stars">
+         <i class="bi bi-star-fill"></i>
+         <i class="bi bi-star-fill"></i>
+        <i class="bi bi-star-fill"></i>
+        <i class="bi bi-star-fill"></i>
+        <i class="bi bi-star-fill"></i>
+        </div>
+         <h3>Digital watch Pro</h3>
+         <h5>R$ 10999,99</h5>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Cum velit reprehenderit reiciendis quae incidunt nostrum ipsa vero autem quisquam libero.</p>
+         <a href="" class="link-btn">Adicionar ao carrinho</a>
+         <a href=""><i class="bi bi-heart"></i>Adicionar a lista e desejos</a>
+    `
+
+
+    img.innerHTML=respostaImg
+    mostrarDados.innerHTML=respostaDados
+    
+})
+
+
+document.querySelector('#img4').addEventListener('click',()=>{
+     respostaDados=''
+ respostaImg=''
+
+    respostaImg+=`
+    <img id="img4" src="./image/product-detail-4.jpg" 
+    alt="relógio preto com o pulso prateado em pé mostrando as horas.">
+    `
+
+    respostaDados+=`
+         <div class="box-stars">
+         <i class="bi bi-star-fill"></i>
+         <i class="bi bi-star-fill"></i>
+        <i class="bi bi-star-fill"></i>
+        <i class="bi bi-star-fill"></i>
+        <i class="bi bi-star-fill"></i>
+        </div>
+         <h3>Digital watch Pro Max</h3>
+         <h5>R$ 11999,99</h5>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Cum velit reprehenderit reiciendis quae incidunt nostrum ipsa vero autem quisquam libero.</p>
+         <a href="" class="link-btn">Adicionar ao carrinho</a>
+         <a href=""><i class="bi bi-heart"></i>Adicionar a lista e desejos</a>
+    `
+
+
+    img.innerHTML=respostaImg
+    mostrarDados.innerHTML=respostaDados
+    
+})
+
+
+
+
